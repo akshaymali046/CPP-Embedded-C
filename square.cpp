f658c16a02b7425b711bd78cb826c3ab33a05181
akshay <akshaymali046@gmail.com>
2023-01-08 02:21:22 +0530

    8/1

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a number"<<endl;
+    cin >>x;
+    int s = x* x;
+    cout<<"square of"<<x<<"is"<<s;
+    getch();
+}
